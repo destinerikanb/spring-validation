e386870d2bf80f56532584b91c41d4784a00fe65
destinerikanb <destinerikanb@gmail.com>
2023-01-05 04:11:54 +0700

    initial commit

---
+++ src/test/java/com/destinerikanb/springvalidation/SpringValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.destinerikanb.springvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
